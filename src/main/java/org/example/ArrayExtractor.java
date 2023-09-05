fc978ba61381a4c6d70fd6d54cf9bce5d6b255d0
Alexandernevsk <Alexandertorreman@gmail.com>
2023-09-05 10:38:34 +0200

    check dependencies

--- src/main/java/org/example/ArrayExtractor.java
+++ src/main/java/org/example/ArrayExtractor.java
@@ -1,5 +1,28 @@
 package org.example;
 
-public class ArrayExtractor {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayExtractor {
+    public static List<String> extract(String JsonArray, String value) {
+        List<String> genres = new ArrayList<>();
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(JsonArray);
+
+            if (rootNode.isArray()) {
+                for (JsonNode genreNode : rootNode) {
+                    String genreName = genreNode.get(value).asText();
+                    genres.add(genreName);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return genres;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,55 @@
-package org.example;public class Main {
+package org.example;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String csvFilePath = "C:\\Users\\Alexa\\Desktop\\TMDB\\tmdb_5000_movies.csv"; // Replace with your CSV file path
+
+
+        try (Reader reader = new FileReader(csvFilePath);
+             CSVParser csvParser = new CSVParser(reader, CSVFormat.EXCEL.withFirstRecordAsHeader())) {
+
+            for (CSVRecord csvRecord : csvParser) {
+                String budget = csvRecord.get("budget");
+                String genres = csvRecord.get("genres");
+                String homepage = csvRecord.get("homepage");
+                String id = csvRecord.get("id");
+                String keywords = csvRecord.get("keywords");
+                String originalLanguage = csvRecord.get("original_language");
+                String originalTitle = csvRecord.get("original_title");
+                String overview = csvRecord.get("overview");
+                String popularity = csvRecord.get("popularity");
+                String productionCompanies = csvRecord.get("production_companies");
+                String productionCountries = csvRecord.get("production_countries");
+                String releaseDate = csvRecord.get("release_date");
+                String revenue = csvRecord.get("revenue");
+                String runtime = csvRecord.get("runtime");
+                String spokenLanguages = csvRecord.get("spoken_languages");
+                String status = csvRecord.get("status");
+                String tagline = csvRecord.get("tagline");
+                String title = csvRecord.get("title");
+                String voteAverage = csvRecord.get("vote_average");
+                String voteCount = csvRecord.get("vote_count");
+
+                System.out.println("Title: " + title + " Genres: " + ArrayExtractor.extract(genres, "name"));
+                System.out.println("Original title : " + originalTitle);
+                System.out.println("tagline: " + tagline);
+                System.out.println("keywords : " + ArrayExtractor.extract(keywords, "name"));
+                System.out.println("overview: " + overview);
+                System.out.println();
+
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
